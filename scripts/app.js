34296c74a688c935147fe7ec2daacc64243ef431
Alexander Korobov <dktwho@mail.ru>
2023-02-06 14:20:38 +0300

    render days

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,11 @@ const page = {
     h1: document.querySelector('.h1'),
     progressPercent: document.querySelector('.progress_percent'),
     progressCoverBar: document.querySelector('.progress__cover_bar'),
+
+  },
+  content: {
+    daysContainer: document.getElementById('days'),
+    nextDay: document.querySelector('.habbit__day')
   }
 
 }
@@ -31,9 +36,7 @@ function saveData() {
 }
 /* render */
 function rerenderMenu(activeHabbit) {
-  if(!activeHabbit) {
-    return 
-  }
+
   for(const habbit of habbits) {
   const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`)
     if(!existed) {
@@ -62,10 +65,24 @@ function rerenderMenu(activeHabbit) {
 
 }
 
-function rerenderHead(activeHabbit) {
-  if(!activeHabbit) {
-    return
+function rerenderContent(activeHabbit) {
+  page.content.daysContainer.innerHTML = ''
+  for(const index in activeHabbit.days) {
+    const element = document.createElement('div')
+    element.classList.add('habbit')
+    element.innerHTML = `    <div class="habbit__day">День ${Number(index) + 1}</div>
+    <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
+    <button class="habbit__delete">
+      <img src="./images/delete.svg" alt="delete ${index + 1}" />
+    </button>`
+    page.content.daysContainer.appendChild(element)
+    page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`
   }
+}
+
+function rerenderHead(activeHabbit) {
+
+
   page.header.h1.innerText = activeHabbit.name;
   const progress = activeHabbit.days.length / activeHabbit.target > 1 
   ? 100
@@ -74,10 +91,16 @@ function rerenderHead(activeHabbit) {
   page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`)
 }
 
+
+
 function rerender(activeHabbitId) {
   const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId)
+  if(!activeHabbit) {
+    return
+  }
   rerenderMenu(activeHabbit)
   rerenderHead(activeHabbit)
+  rerenderContent(activeHabbit)
 }
 
 /* init */
